5457450ee3fa8defd7a675d9e382f1199cfa8479
Yurii <yuramasha13@gmail.com>
2022-09-25 13:22:18 +0300

    Adds corrections

--- contacts.js
+++ contacts.js
@@ -1,7 +1,10 @@
-const fs = require('fs').promises;
-const path = require('path');
-const contactsPath = path.resolve(__dirname, './db/contacts.json');
+const fs = require("fs").promises;
+const path = require("path");
+const { nanoid } = require("nanoid");
+const contactsPath = path.join(__dirname, "./db/contacts.json");
 
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
 
 const listContacts = async () => {
   const data = await fs.readFile(contactsPath);
@@ -9,34 +12,38 @@ const listContacts = async () => {
   return contacts;
 };
 
-const getContactsById = async contactId => {
+const getContactsById = async (contactId) => {
   const contacts = await listContacts();
   contactId = String(contactId);
-  const contact = contacts.find(c => c.id === contactId);
- 
-  return contact || null ;
+  const contact = contacts.find((c) => c.id === contactId);
+
+  return contact || null;
 };
 
-const addContact = async (name, email, phone, id) => {
+const addContact = async (name, email, phone) => {
   const contacts = await listContacts();
-  const newId = String(Number([...contacts].sort((a, b) => b.id - a.id)[0].id) + 1);
-  const newContact = { name, email, phone, id: newId };
+  const newContact = {
+    id: nanoid(4),
+    name,
+    email,
+    phone,
+  };
   contacts.push(newContact);
   await fs.writeFile(contactsPath, JSON.stringify(contacts));
   return newContact;
 };
 
-const removeContact = async contactId => {
+const removeContact = async (contactId) => {
   const contacts = await listContacts();
   contactId = String(contactId);
-  const contactIndex = contacts.findIndex(c => c.id === contactId);
+  const contactIndex = contacts.findIndex((c) => c.id === contactId);
   if (contactIndex === -1) {
     return null;
   }
 
-  const updatedContacts = contacts.filter((_, index) => index !== contactIndex);
-  await fs.writeFile(contactsPath, JSON.stringify(updatedContacts));
-  return contacts[contactIndex];
+  const [result] = contacts.splice(contactIndex, 1);
+  await updateContacts(contacts);
+  return result;
 };
 
 module.exports = {
